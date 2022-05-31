edacc8fe2af53bbc9243dd77044fbc3e00d6f980
Miron <alexander.mironov.jr@ya.ru>
2022-05-31 16:25:21 +0300

    First commit!

---
+++ src/main/java/ru/mironov/currentexchangerate/CurrentExchangeRateApplication.java
@@ -0,0 +1,13 @@
+package ru.mironov.currentexchangerate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrentExchangeRateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrentExchangeRateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/mironov/currentexchangerate/CurrentExchangeRateApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mironov.currentexchangerate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrentExchangeRateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
